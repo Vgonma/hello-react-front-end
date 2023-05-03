9eeaa5c0c6c661624665c878f5f14c8aaea7b59e
Vgonma <victorgmassimi.microverse@gmail.com>
2023-05-02 18:45:29 -0600

    Moved store to separate file inside redux folder.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux'
-import greetingReducer from './redux/greetingSlice'
 import App from './App.jsx'
 import './index.css'
-
-const store = configureStore({
-  reducer: {
-    greeting: greetingReducer
-  }
-});
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
